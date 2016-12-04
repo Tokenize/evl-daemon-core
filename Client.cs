4fd25254d0717d12eb33de97c16bb91e049cb16c
Mike Farr <mike@tokenize.ca>
2016-12-04 12:28:48 -0500

    Add Client class with login commands defined

---
+++ Client.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EvlDaemon
+{
+    public class Client
+    {
+        public static string LoginPasswordRequest = "5053";
+        public static string LoginTimedOut = "5052";
+        public static string LoginFail = "5050";
+        public static string LoginSuccessful = "5051";
+
+        public static Dictionary<string, string> CommandDescriptions = new Dictionary<string, string>()
+        {
+            { LoginPasswordRequest, "Login Password Request" },
+            { LoginTimedOut, "Login Timed Out" },
+            { LoginFail, "Login Failed" },
+            { LoginSuccessful, "Login Successful" }
+        };
+
+        public static string Describe(string command)
+        {
+            return CommandDescriptions.ContainsKey(command) ? CommandDescriptions[command] : command;
+        }
+    }
+}
