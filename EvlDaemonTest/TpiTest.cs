257f8c1a40c6127e7b2b63c97fc21e83b7243e82
Mike Farr <mike@tokenize.ca>
2016-12-09 12:51:13 -0500

    Add EvlDaemonTest project

---
+++ EvlDaemonTest/TpiTest.cs
@@ -0,0 +1,51 @@
+﻿using EvlDaemon;
+using Xunit;
+
+namespace EvlDaemonTest
+{
+    public class TpiTest
+    {
+        [Fact]
+        public void ChecksumIsCorrect()
+        {
+            Assert.Equal("D2", Tpi.CalculateChecksum("6543"));
+        }
+
+        [Fact]
+        public void ChecksumTruncatedToOneByte()
+        {
+            Assert.Equal("CA", Tpi.CalculateChecksum("005123456"));
+        }
+
+        [Fact]
+        public void ChecksumLeadingZeroAdded()
+        {
+            Assert.Equal("0F", Tpi.CalculateChecksum("5108A"));
+        }
+
+        [Fact]
+        public void GetCommandReturnsCorrectPart()
+        {
+            Assert.Equal("005", Tpi.GetCommand("005user54"));
+        }
+
+        [Fact]
+        public void GetDataReturnsCorrectPart()
+        {
+            Assert.Equal("user", Tpi.GetData("005user54"));
+        }
+
+        [Fact]
+        public void GetDataReturnsEmptyString()
+        {
+            Assert.Equal("", Tpi.GetData("5108A"));
+        }
+
+        [Fact]
+        public void GetChecksumReturnsCorrectPart()
+        {
+            Assert.Equal("54", Tpi.GetChecksum("005user54"));
+        }
+
+    }
+}
