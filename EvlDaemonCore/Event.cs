6c509fb0c38dbc4f3a6847cbf5daa5372d1f6fa1
Mike Farr <mike@tokenize.ca>
2017-01-12 09:42:50 -0500

    Improve command/data description functionality

--- EvlDaemonCore/Event.cs
+++ EvlDaemonCore/Event.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace EvlDaemon
 {
@@ -21,17 +19,40 @@ namespace EvlDaemon
         public const string PartitionDisarmed = "655";
         public const string ExitDelayInProgress = "656";
         public const string EntryDelayInProgress = "657";
+        public const string PartitionIsBusy = "673";
         public const string SpecialClosing = "701";
         public const string UserOpening = "750";
+        public const string TroubleLedOff = "841";
 
         public const string Login = "505";
         public static class LoginData
         {
-            public static string IncorrectPassword = "0";
-            public static string LoginSuccessful = "1";
-            public static string TimeOut = "2";
-            public static string PasswordRequest = "3";
+            public const string IncorrectPassword = "0";
+            public const string LoginSuccessful = "1";
+            public const string TimeOut = "2";
+            public const string PasswordRequest = "3";
         }
+        public static Dictionary<string, string> LoginDescriptions = new Dictionary<string, string>()
+        {
+            { LoginData.IncorrectPassword, "Incorrect Password" },
+            { LoginData.LoginSuccessful, "Login Successful" },
+            { LoginData.TimeOut, "Time Out" },
+            { LoginData.PasswordRequest, "Password Request" }
+        };
+
+        public static Dictionary<string, string> PartitionDescriptions = new Dictionary<string, string>()
+        {
+            // TODO: Replace sample partition descriptions with ones read from configuration
+            { "1", "Main Partition" }
+        };
+
+        public static Dictionary<string, string> ZoneDescriptions = new Dictionary<string, string>()
+        {
+            // TODO: Replace sample zone descriptions with ones read from configuration
+            { "001", "Front Door" },
+            { "002", "Garage Door" },
+            { "003", "Back Door" }
+        };
 
         public static List<Command> Commands = new List<Command>
         {
@@ -67,22 +88,65 @@ namespace EvlDaemon
                 Description = "Zone Restored", Priority = Command.PriorityLevel.Medium } }
         };
 
-        public static string Describe(string command)
+        /// <summary>
+        /// Returns a friendly description of the given command and data strings.
+        /// </summary>
+        /// <param name="command">Command to describe</param>
+        /// <param name="data">Data to describe</param>
+        /// <returns>Friendly descriotion of command and data</returns>
+        public static string Describe(string command, string data = "")
         {
             Command cmd = GetCommand(command);
-            return Describe(cmd);
+            return Describe(cmd, data);
         }
 
-        public static string Describe(Command command)
+        /// <summary>
+        /// Returns a friendly description of the given command and data.
+        /// </summary>
+        /// <param name="command">Command to describe</param>
+        /// <param name="data">Data to describe</param>
+        /// <returns>Friendly description of command and data</returns>
+        public static string Describe(Command command, string data = "")
         {
             if (string.IsNullOrEmpty(command.Description))
             {
-                return "Unknown command: " + command.Number;
+                return string.Format("Unknown command ({0})", command.Number);
             }
 
-            return command.Description;
+            string description;
+            switch (command.Number)
+            {
+                case Login:
+                    description = string.Format("{0}: {1}", command.Description, DescribeData(data, LoginDescriptions));
+                    break;
+                case PartitionReady:
+                case PartitionNotReady:
+                case PartitionArmed:
+                case PartitionInAlarm:
+                case PartitionDisarmed:
+                case ExitDelayInProgress:
+                case EntryDelayInProgress:
+                case PartitionIsBusy:
+                case SpecialClosing:
+                    description = string.Format("{0}: {1}", command.Description, DescribeData(data, PartitionDescriptions));
+                    break;
+                case ZoneOpen:
+                case ZoneRestored:
+                    description = string.Format("{0}: {1}", command.Description, DescribeData(data, ZoneDescriptions));
+                    break;
+                default:
+                    description = command.Description;
+                    break;
+            }
+
+            return description;
         }
 
+        /// <summary>
+        /// Returns the Command object that represents the given command string.
+        /// </summary>
+        /// <param name="command">Command string</param>
+        /// <returns>Command object that represents given command string</returns>
         public static Command GetCommand(string command)
         {
             Command cmd = Commands.FirstOrDefault(c => c.Number == command);
@@ -93,6 +157,28 @@ namespace EvlDaemon
             }
 
             return cmd;
+        }
+
+        /// <summary>
+        /// Returns a friendly description of the given data from the given dictionary of descriptions.
+        /// </summary>
+        /// <param name="data">Data to describe</param>
+        /// <param name="descriptions">Dictionary of description strings</param>
+        /// <returns>Friendly description of data</returns>
+        private static string DescribeData(string data, Dictionary<string, string> descriptions)
+        {
+            string description;
+
+            if (descriptions.ContainsKey(data))
+            {
+                description = descriptions[data];
+            }
+            else
+            {
+                description = string.Format("Unknown ({0})", data);
+            }
+
+            return description;
         }
     }
 }
---
+++ EvlDaemonTest/EventTest.cs
@@ -0,0 +1,31 @@
+﻿using EvlDaemon;
+using Xunit;
+
+namespace EvlDaemonTest
+{
+    public class EventTest
+    {
+        [Fact]
+        public void LoginIncorrectPasswordDescriptionIsCorrect()
+        {
+            Command command = Event.GetCommand(Event.Login);
+            Assert.Equal("Login Interaction: Incorrect Password",
+                Event.Describe(command, Event.LoginData.IncorrectPassword));
+        }
+
+        [Fact]
+        public void LoginSuccessfulDescriptionIsCorrect()
+        {
+            Command command = Event.GetCommand(Event.Login);
+            Assert.Equal("Login Interaction: Login Successful",
+                Event.Describe(command, Event.LoginData.LoginSuccessful));
+        }
+
+        [Fact]
+        public void UnknownCommandDescriptionIsCorrect()
+        {
+            Command command = new Command() { Number = "999" };
+            Assert.Equal("Unknown command (999)", Event.Describe(command));
+        }
+    }
+}
